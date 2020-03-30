4adf9ee73e1a678ee545e15593b246528f3c59aa
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2020-03-30 13:02:22 +0100

    unicafe step3

--- Part01/unicafe/src/index.js
+++ Part01/unicafe/src/index.js
@@ -5,14 +5,28 @@ const Button = props => {
   return <button onClick={props.onClickEvent}>{props.text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+  const totalSum = good + neutral + bad;
+  const weighedTotal = good * 1 + neutral * 0 + bad * -1;
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {totalSum}</p>
+      <p>average {totalSum === 0 ? 0 : weighedTotal / totalSum}</p>
+      <p>positive {totalSum === 0 ? 0 : (good / totalSum) * 100 + "%"}</p>
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const totalSum = good + neutral + bad;
-  const weighedTotal = good * 1 + neutral * 0 + bad * -1;
 
   return (
     <div>
@@ -23,13 +37,7 @@ const App = () => {
         text="neutral"
       ></Button>
       <Button onClickEvent={() => setBad(bad + 1)} text="bad"></Button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {totalSum}</p>
-      <p>average {totalSum === 0 ? 0 : weighedTotal / totalSum}</p>
-      <p>positive {totalSum === 0 ? 0 : (good / totalSum) * 100 + "%"}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
